1c873bdb5de74ac04d16052303e244bf85b0280a
pavel <pavel.deleersnyder@student.howest.be>
2024-12-04 14:27:15 +0100

    create basic board with checks for intersections

---
+++ domain/Board.cs
@@ -0,0 +1,40 @@
+﻿namespace domain;
+
+public class Board
+{
+    private readonly int _width = 10;
+    private readonly int _height = 20;
+
+    private int[][] _matrix;
+
+    public Board()
+    {
+        InitBoard();
+    }
+
+    private void InitBoard()
+    {
+        _matrix = new int[_width][];
+        for (int i = 0; i < _width; i++)
+        {
+            _matrix[i] = new int[_height * 2];
+        }
+    }
+
+    public bool IntersectPiece(Piece piece)
+    {
+        Block[] blocks = piece.GetBlocks();
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            if (IntersectBlock(blocks[i].X + piece.X, blocks[i].Y + piece.Y)) return true;
+        }
+        return false;
+    }
+
+    public bool IntersectBlock(int x, int y)
+    {
+        if (x < 0 || y < 0) return true;
+        if (x > _width) return true;
+        return _matrix[x][y] != 0;
+    }
+}
