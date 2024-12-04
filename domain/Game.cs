c5dcf9125e12bf9a91fb3ea777f68678142c7178
pavel <pavel.deleersnyder@student.howest.be>
2024-12-04 15:38:09 +0100

    create container game to easily manage the combination of a board, piece and renderer

---
+++ domain/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace domain;
+
+public class Game
+{
+    public Board Board { get; set; }
+    public ActivePiece PlayerPiece { get; set; }
+    public BoardRenderer Renderer { get; set; }
+
+    public Game()
+    {
+        this.Board = new Board();
+        this.PlayerPiece = new ActivePiece(this.Board);
+        this.Renderer = new BoardRenderer(this.Board, this.PlayerPiece);
+    }
+}
