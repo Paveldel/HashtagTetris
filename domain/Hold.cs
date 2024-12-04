5b788eba6a905c189749ed49a69e0b2fb3405a02
pavel <pavel.deleersnyder@student.howest.be>
2024-12-04 21:27:44 +0100

    create hold object for storing hold logic

---
+++ domain/Hold.cs
@@ -0,0 +1,26 @@
+﻿namespace domain;
+
+public class Hold
+{
+    private int _heldPieceIndex = 0;
+    private bool _holdEnabled = true;
+
+    public Piece? HoldPiece(Piece p)
+    {
+        if (!_holdEnabled) return p;
+        int temp = _heldPieceIndex;
+        _heldPieceIndex = p.GetPieceIndex();
+        _holdEnabled = false;
+        return PieceData.Pieces[temp];
+    }
+
+    public bool IsEnabled()
+    {
+        return _holdEnabled;
+    }
+
+    public int GetHeldPieceType()
+    {
+        return _heldPieceIndex;
+    }
+}
