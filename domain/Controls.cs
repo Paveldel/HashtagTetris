5cf98829d6a02c8d1171756505ef63e8099b5522
pavel <pavel.deleersnyder@student.howest.be>
2024-12-04 22:28:30 +0100

    create object to hold control config

---
+++ domain/Controls.cs
@@ -0,0 +1,13 @@
+﻿namespace domain;
+
+public class Controls
+{
+    public static readonly string MoveLeft = "Numpad1";
+    public static readonly string MoveRight = "Numpad3";
+    public static readonly string SoftDrop = "Numpad2";
+    public static readonly string HardDrop = "Space";
+    public static readonly string Clockwise = "KeyR";
+    public static readonly string Reverse = "KeyE";
+    public static readonly string AntiClockwise = "KeyW";
+    public static readonly string Hold = "Numpad5";
+}
