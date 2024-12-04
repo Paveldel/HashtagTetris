ace771070a2b0d816d554afc0c1074f2e74173d2
pavel <pavel.deleersnyder@student.howest.be>
2024-12-04 14:17:06 +0100

    create pieceData for quick access

---
+++ domain/PieceData.cs
@@ -0,0 +1,15 @@
+﻿namespace domain;
+
+public class PieceData
+{
+    public static readonly Piece[] Pieces = [
+        null!,
+        new Piece([new Block(0, 0, true), new Block(1, 0, true), new Block(-1, 0, true), new Block(-2, 0, true)], 0, 0, PieceType.I),
+        new Piece([new Block(0, 0, false), new Block(1, 0, false), new Block(-1, 0, false), new Block(0, 1, false)], 0, 0, PieceType.T),
+        new Piece([new Block(0, 0, true), new Block(1, 0, true), new Block(0, -1, true), new Block(1, -1, true)], 0, 0, PieceType.O),
+        new Piece([new Block(0, 0, false), new Block(1, 0, false), new Block(-1, 0, false), new Block(1, 1, false)], 0, 0, PieceType.L),
+        new Piece([new Block(0, 0, false), new Block(1, 0, false), new Block(-1, 0, false), new Block(-1, 1, false)], 0, 0, PieceType.J),
+        new Piece([new Block(0, 0, false), new Block(0, 1, false), new Block(-1, 0, false), new Block(1, 1, false)], 0, 0, PieceType.S),
+        new Piece([new Block(0, 0, false), new Block(1, 0, false), new Block(0, 1, false), new Block(-1, 1, false)], 0, 0, PieceType.Z),
+    ];
+}
