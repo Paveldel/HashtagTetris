008e967d8c9989c11ba25eae7ab860a461d5c3ef
pavel <pavel.deleersnyder@student.howest.be>
2024-12-04 15:35:33 +0100

    create renderer for handling all render related functions

---
+++ domain/BoardRenderer.cs
@@ -0,0 +1,25 @@
+﻿namespace domain;
+
+public class BoardRenderer
+{
+    private readonly Board _board;
+    private readonly ActivePiece _piece;
+
+    public BoardRenderer(Board board, ActivePiece piece)
+    {
+        _board = board;
+        _piece = piece;
+    }
+
+    public int[][] BoardToRender()
+    {
+        int[][] result = _board.GetMatrixCopy();
+        Piece piece = _piece.GetPiece();
+        Block[] blocks = piece.GetBlocks();
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            result[blocks[i].X + piece.X][blocks[i].Y + piece.Y] = piece.GetPieceIndex();
+        }
+        return result;
+    }
+}
