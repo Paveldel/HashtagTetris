1c46a494cf07a27214081b5946f6289249a6205d
pavel <pavel.deleersnyder@student.howest.be>
2024-12-05 18:09:10 +0100

    add timers to for handling int Input

--- domain/Input.cs
+++ domain/Input.cs
@@ -2,19 +2,28 @@
 
 public class Input(ActivePiece playerPiece)
 {
+    private long _leftTimer = long.MaxValue;
+    private long _rightTimer = long.MaxValue;
+    private long _softDropTimer = long.MaxValue;
+    
     public void HandleKeyPress(string keyCode)
     {
         if (keyCode == Controls.MoveLeft)
         {
             playerPiece.MoveLeft();
+            _leftTimer = getCurrentTime() + Controls.DAS;
+            _rightTimer = long.MaxValue;
         }
         else if (keyCode == Controls.SoftDrop)
         {
             playerPiece.MoveDown();
+            _softDropTimer = getCurrentTime() + Controls.SDF;
         }
         else if (keyCode == Controls.MoveRight)
         {
             playerPiece.MoveRight();
+            _rightTimer = getCurrentTime() + Controls.DAS;
+            _leftTimer = long.MaxValue;
         }
         else if (keyCode == Controls.Hold)
         {
@@ -40,6 +49,27 @@ public class Input(ActivePiece playerPiece)
     
     public void HandleKeyRelease(string keyCode)
     {
+        if (keyCode == Controls.MoveLeft)
+        {
+            _leftTimer = long.MaxValue;
+        }
+        else if (keyCode == Controls.SoftDrop)
+        {
+            _softDropTimer = long.MaxValue;
+        }
+        else if (keyCode == Controls.MoveRight)
+        {
+            _rightTimer = long.MaxValue;
+        }
+    }
+
+    public void update()
+    {
         
+    }
+
+    private long getCurrentTime()
+    {
+        return DateTimeOffset.Now.ToUnixTimeMilliseconds();
     }
 }
