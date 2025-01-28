52fe8fce71e8daabc0b8064e8c058ccf3b9d61c7
pavel <pavel.deleersnyder@student.howest.be>
2025-01-28 10:17:44 +0100

    fix very broken damage calculator

--- domain/GuideLineDamageCalculator.cs
+++ domain/GuideLineDamageCalculator.cs
@@ -6,21 +6,33 @@ public class GuideLineDamageCalculator : IDamageCalculator
     private const int NoBackToBack = 0;
     private const int BackToBack = 1;
     private const int PerfectClearDamage = 10;
-    private static readonly int[] ComboDamage = new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 5};
-    private static readonly int[] LineClearDamage = new[] { 0, 0, 1, 2, 4};
+    private static readonly int[] ComboDamage = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 5};
+    private static readonly int[] LineClearDamage = { 0, 0, 1, 2, 4};
     
     private int _combo = NoCombo;
     private int _backToBack = NoBackToBack;
     
     public int CalculateDamage(int linesCleared, SpinType spinType, bool isPerfectClear)
     {
-        int amountOfDamage = GetLineClearDamage(linesCleared, spinType);
-        UpdateCombo(linesCleared);
-        amountOfDamage += ComboDamage[Math.Min(_combo, ComboDamage.Length - 1)];
-        amountOfDamage += _backToBack;
-        UpdateBackToBack(linesCleared, spinType);
-        if (isPerfectClear) amountOfDamage += PerfectClearDamage;
-        return amountOfDamage;
+        int resultDamage = 0;
+        if (linesCleared == 0) HandleNoLineClear();
+        else resultDamage = HandleLineClear(linesCleared, spinType);
+        if (isPerfectClear) resultDamage += PerfectClearDamage;
+        return resultDamage;
+    }
+    
+    private void HandleNoLineClear()
+    {
+        _combo = NoCombo;
+    }
+
+    private int HandleLineClear(int linesCleared, SpinType spinType)
+    {
+        _combo++;
+        int resultDamage = GetLineClearDamage(linesCleared, spinType) 
+                           + GetComboDamage()
+                           + GetBackToBackDamage(linesCleared, spinType);
+        return resultDamage;
     }
 
     private int GetLineClearDamage(int linesCleared, SpinType spinType)
@@ -28,19 +40,27 @@ public class GuideLineDamageCalculator : IDamageCalculator
         if (spinType == SpinType.FullSpin) return linesCleared * 2;
         return LineClearDamage[Math.Min(linesCleared, LineClearDamage.Length - 1)];
     }
-
-    private void UpdateBackToBack(int linesCleared, SpinType spinType)
+    
+    private int GetComboDamage()
     {
-        if (linesCleared == 0) return;
-        if (linesCleared == 4 || spinType == SpinType.MiniSpin || spinType == SpinType.FullSpin)
-            _backToBack = BackToBack;
-        else _backToBack = NoBackToBack;
+        return ComboDamage[Math.Min(_combo, ComboDamage.Length - 1)];
     }
 
-    private void UpdateCombo(int linesCleared)
+    private int GetBackToBackDamage(int linesCleared, SpinType spinType)
     {
-        if (linesCleared == 0) _combo = NoCombo;
-        else _combo++;
+        if (IsSpecialClear(linesCleared, spinType))
+        {
+            int hadBackToBack = _backToBack;
+            _backToBack = BackToBack;
+            return hadBackToBack;
+        }
+        _backToBack = NoBackToBack;
+        return NoBackToBack;
+    }
+
+    private bool IsSpecialClear(int linesCleared, SpinType spinType)
+    {
+        return (linesCleared > 3) || spinType != SpinType.NoSpin;
     }
 
     public int GetCombo()
