9a54034c0b9cde245806f027ac5132ac5a92267b
pavel <pavel.deleersnyder@student.howest.be>
2025-01-30 13:29:44 +0100

    create class to hold logic for what to do with received garbage

---
+++ domain/IDamageQueue.cs
@@ -0,0 +1,9 @@
+﻿namespace domain;
+
+public interface IDamageQueue
+{
+    public void RegisterAsReceiver(IDamageReceiver receiver);
+    public List<int> GetQueue();
+    public void PiecePlaced(int amountOfDamageSent, bool lineCleared);
+    public void AddGarbageToQueue(int amountOfGarbage);
+}
