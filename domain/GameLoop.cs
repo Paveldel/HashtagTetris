b963bb5ecbc366fff3970f52003713748dbc0b68
pavel <pavel.deleersnyder@student.howest.be>
2025-02-01 10:40:05 +0100

    make game loop dynamically call all updatables

--- domain/GameLoop.cs
+++ domain/GameLoop.cs
@@ -1,19 +1,41 @@
 ﻿namespace domain;
 
-public class GameLoop(Input input, Gravity gravity)
+public class GameLoop() : ITimer
 {
     private const int TargetFps = 60;
     private const int Milliseconds = 1000;
     private bool _gameover = false;
-    private Input _playerInput = input;
-    public async Task StartLoop(IScreen updateCallback)
+    private List<IUpdatable> _updatables = new ();
+    
+    public async Task StartLoop()
     {
         while (!_gameover)
         {
             await Task.Delay(Milliseconds / TargetFps);
-            _playerInput.Update();
-            gravity.Update();
-            updateCallback.Rerender();
+            Update();
+        }
+    }
+
+    private void Update()
+    {
+        long currentTime = GetCurrentTime();
+        foreach (var updatable in _updatables)
+        {
+            updatable.Update(currentTime);
+        }
+    }
+
+    public long GetCurrentTime()
+    {
+        return DateTimeOffset.Now.ToUnixTimeMilliseconds();
+    }
+
+    public void RegisterUpdatable(params IUpdatable[] updatables)
+    {
+        foreach (var updatable in updatables)
+        {
+            updatable.SetTimer(this);
+            _updatables.Add(updatable);
         }
     }
 }
