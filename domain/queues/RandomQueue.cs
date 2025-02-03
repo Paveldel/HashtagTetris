721429c100c18ec4cc49bab4b67a344758183a83
pavel <pavel.deleersnyder@student.howest.be>
2025-02-03 14:00:15 +0100

    create completely random queue

---
+++ domain/queues/RandomQueue.cs
@@ -0,0 +1,24 @@
+﻿using domain.piecedata;
+
+namespace domain.queues;
+
+public class RandomQueue : AbstractQueue
+{
+    private const int _amountOfStartingPieces = 50;
+    
+    private readonly Random _random = new();
+
+    public RandomQueue(IPieceData pieceData) : base(pieceData)
+    {
+        for (int i = 0; i < _amountOfStartingPieces; i++)
+        {
+            AddPiece();
+        }
+    }
+
+    public override void AddPiece()
+    {
+        int randomPiece = _random.Next(1, PieceData.GetAmountOfPieces());
+        Queue.Add(PieceData.GetPieceByIndex(randomPiece)!);
+    }
+}
