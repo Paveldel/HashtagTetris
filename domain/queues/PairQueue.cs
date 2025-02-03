0871c7fc443e74d5354fcbd3dc908aaa3655b606
pavel <pavel.deleersnyder@student.howest.be>
2025-02-03 14:18:27 +0100

    create weird queue which generates pieces in pairs

---
+++ domain/queues/PairQueue.cs
@@ -0,0 +1,56 @@
+﻿using domain.data;
+using domain.piecedata;
+
+namespace domain.queues;
+
+public class PairQueue : AbstractQueue
+{
+    private const int AmountOfBatchesToStart = 10;
+    private const int AmountOfPairsPerBatch = 4;
+    
+    private readonly Random _random = new();
+
+    public PairQueue(IPieceData pieceData) : base(pieceData)
+    {
+        for (int i = 0; i < AmountOfBatchesToStart; i++)
+        {
+            AddPiece();
+        }
+    }
+
+    public override void AddPiece()
+    {
+        if (Queue.Count > 100) return;
+        List<Piece> pairs = GeneratePairs();
+        while (pairs.Count != 0)
+        {
+            PutRandomPieceInQueue(pairs);
+        }
+    }
+
+    private List<Piece> GeneratePairs()
+    {
+        List<Piece> pair = new();
+        int firstPiece = _random.Next(1, PieceData.GetAmountOfPieces());
+        int secondPiece = _random.Next(1, PieceData.GetAmountOfPieces() - 1);
+        if (secondPiece >= firstPiece) secondPiece++;
+        AddPieceToList(pair, firstPiece);
+        AddPieceToList(pair, secondPiece);
+        return pair;
+    }
+
+    private void AddPieceToList(List<Piece> list, int secondPiece)
+    {
+        for (int i = 0; i < AmountOfPairsPerBatch; i++)
+        {
+            list.Add(PieceData.GetPieceByIndex(secondPiece)!);
+        }
+    }
+
+    private void PutRandomPieceInQueue(List<Piece> bag)
+    {
+        int randomIndex = _random.Next(0, bag.Count);
+        Queue.Add(bag.ElementAt(randomIndex));
+        bag.RemoveAt(randomIndex);
+    }
+}
