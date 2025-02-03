ec07bdc15fc3fd58f1299bd172bea900584e7295
pavel <pavel.deleersnyder@student.howest.be>
2025-02-03 14:07:10 +0100

    create queue that mimics classic NES tetris

---
+++ domain/queues/ClassicQueue.cs
@@ -0,0 +1,32 @@
+﻿using domain.piecedata;
+
+namespace domain.queues;
+
+public class ClassicQueue : AbstractQueue
+{
+    private const int AmountOfStartingPieces = 50;
+    private int _lastGeneratedPiece = 0;
+    
+    private readonly Random _random = new();
+    
+    public ClassicQueue(IPieceData pieceData) : base(pieceData)
+    {
+        for (int i = 0; i < AmountOfStartingPieces; i++)
+        {
+            AddPiece();
+        }
+    }
+
+    public override void AddPiece()
+    {
+        int randomPiece = _random.Next(0, PieceData.GetAmountOfPieces());
+        if (randomPiece == 0 || randomPiece == _lastGeneratedPiece) randomPiece = SecondPass();
+        Queue.Add(PieceData.GetPieceByIndex(randomPiece)!);
+        _lastGeneratedPiece = randomPiece;
+    }
+
+    private int SecondPass()
+    {
+        return _random.Next(1, PieceData.GetAmountOfPieces());
+    }
+}
