460ba1b9e15a141307d9a43cb0fc51db77c54102
pavel <pavel.deleersnyder@student.howest.be>
2025-02-04 12:50:52 +0100

    allow spins to be detected with Immobile

---
+++ domain/spindetectors/Immobile.cs
@@ -0,0 +1,37 @@
+﻿using domain.data;
+
+namespace domain.spindetectors;
+
+public class Immobile : ISpinDetector
+{
+    public SpinType DetectSpin(Piece piece, Board board, int lastKick)
+    {
+        if (CanPieceMoveUp(piece, board)) return SpinType.NoSpin;
+        if (CanPieceMoveLeft(piece, board)) return SpinType.NoSpin;
+        return CanPieceMoveRight(piece, board) ? SpinType.NoSpin : SpinType.FullSpin;
+    }
+
+    private bool CanPieceMoveLeft(Piece piece, Board board)
+    {
+        piece.MoveLeft();
+        bool result = !board.IntersectPiece(piece);
+        piece.MoveRight();
+        return result;
+    }
+    
+    private bool CanPieceMoveRight(Piece piece, Board board)
+    {
+        piece.MoveRight();
+        bool result = !board.IntersectPiece(piece);
+        piece.MoveLeft();
+        return result;
+    }
+    
+    private bool CanPieceMoveUp(Piece piece, Board board)
+    {
+        piece.Y++;
+        bool result = !board.IntersectPiece(piece);
+        piece.Y--;
+        return result;
+    }
+}
