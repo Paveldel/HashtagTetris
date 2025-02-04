4d1acd1ee54e64768f35152614b347e8a0cca26c
pavel <pavel.deleersnyder@student.howest.be>
2025-02-04 13:55:10 +0100

    create SRSX with more powerful reverse kick table

---
+++ domain/rotationsystem/SRSX.cs
@@ -0,0 +1,36 @@
+﻿using domain.data;
+
+namespace domain.rotationsystem;
+
+public class SRSX : SRS
+{
+    
+    private static readonly Kick[][] NormalReverseKicks = new[]
+    {
+        new[] { new Kick(0, 0), new Kick(1, 0), new Kick(2, 0), new Kick(1, 1), new Kick(2, 1), new Kick(-1, 0), new Kick(-2, 0), new Kick(-1, 1), new Kick(-2, 1), new Kick(0, -1), new Kick(3, 0), new Kick(-3, 0) },
+        new[] { new Kick(0, 0), new Kick(0, 1), new Kick(0, 2), new Kick(-1, 1), new Kick(-1, 2), new Kick(0, -1), new Kick(0, -2), new Kick(-1, -1), new Kick(-1, -2), new Kick(1, 0), new Kick(0, 3), new Kick(0, -3) },
+        new[] { new Kick(0, 0), new Kick(-1, 0), new Kick(-2, 0), new Kick(-1, -1), new Kick(-2, -1), new Kick(1, 0), new Kick(2, 0), new Kick(1, -1), new Kick(2, -1), new Kick(0, 1), new Kick(-3, 0), new Kick(3, 0) },
+        new[] { new Kick(0, 0), new Kick(0, 1), new Kick(0, 2), new Kick(1, 1), new Kick(1, 2), new Kick(0, -1), new Kick(0, -2), new Kick(1, -1), new Kick(1, -2), new Kick(-1, 0), new Kick(0, 3), new Kick(0, -3) }
+    };
+    
+    private static readonly Kick[][] IReverseKicks = new[]
+    {
+        new[] { new Kick(0, 0), new Kick(-1, 0), new Kick(-2, 0), new Kick(1, 0), new Kick(2, 0), new Kick(0, 1) },
+        new[] { new Kick(0, 0), new Kick(0, 1), new Kick(0, 2), new Kick(0, -1), new Kick(0, -2), new Kick(-1, 0) },
+        new[] { new Kick(0, 0), new Kick(1, 0), new Kick(2, 0), new Kick(-1, 0), new Kick(-2, 0), new Kick(0, -1) },
+        new[] { new Kick(0, 0), new Kick(0, 1), new Kick(0, 2), new Kick(0, -1), new Kick(0, -2), new Kick(1, 0) },
+    };
+    
+    protected override Piece RotateReverse(Piece piece, Board board)
+    {
+        Piece rotatedPiece = piece.Rotate(Rotation.Reverse);
+        TryKicks(rotatedPiece, board, GetReverseKicks(piece));
+        return KickIndex == FailedRotation ? piece : rotatedPiece;
+    }
+
+    private Kick[] GetReverseKicks(Piece piece)
+    {
+        if (piece.GetPieceIndex() == (int)PieceType.I) return IReverseKicks[piece.RotIndex];
+        return NormalReverseKicks[piece.RotIndex];
+    }
+}
