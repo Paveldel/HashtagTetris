67452c83bb594a9af8fa4e556cd64d83e1943871
pavel <pavel.deleersnyder@student.howest.be>
2025-02-04 12:54:53 +0100

    create standard spin detector which only allows T spins

---
+++ domain/spindetectors/OnlyT.cs
@@ -0,0 +1,14 @@
+﻿using domain.data;
+
+namespace domain.spindetectors;
+
+public class OnlyT : ISpinDetector
+{
+    private ISpinDetector _spinDetector = new FourCorner();
+    
+    public SpinType DetectSpin(Piece piece, Board board, int lastKick)
+    {
+        if (piece.GetPieceIndex() != (int)PieceType.T) return SpinType.NoSpin;
+        return _spinDetector.DetectSpin(piece, board, lastKick);
+    }
+}
