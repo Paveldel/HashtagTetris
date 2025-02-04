b24b5564935080ac4e63a9480deffea57ed721de
pavel <pavel.deleersnyder@student.howest.be>
2025-02-04 15:46:33 +0100

    create rotation system which does not support any kicks

---
+++ domain/rotationsystem/NoKicks.cs
@@ -0,0 +1,18 @@
+﻿using domain.data;
+
+namespace domain.rotationsystem;
+
+public class NoKicks : IRotationSystem
+{
+    public Piece RotatePiece(Piece piece, Board board, Rotation rotation)
+    {
+        Piece rotatedPiece = piece.Rotate(rotation);
+        if (!board.IntersectPiece(rotatedPiece)) return rotatedPiece;
+        return piece;
+    }
+
+    public int GetLastUsedKickIndex()
+    {
+        return 0;
+    }
+}
