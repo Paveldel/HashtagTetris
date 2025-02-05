bcd4c4512633fb306b97cfc37ba09bc8ecf4d62e
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 16:55:09 +0100

    return damage calculator which does not allow any damage

---
+++ domain/damage/calculator/NoDamageCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace domain.damage.calculator;
+
+public class NoDamageCalculator : IDamageCalculator
+{
+    public int CalculateDamage(int linesCleared, SpinType spinType, bool isPerfectClear)
+    {
+        return 0;
+    }
+
+    public int GetCombo()
+    {
+        return -1;
+    }
+
+    public int GetBackToBack()
+    {
+        return 0;
+    }
+}
