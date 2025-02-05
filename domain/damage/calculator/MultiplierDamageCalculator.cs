32eb820fbaccf2cde8527c81f3fca6ff97d39d59
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 16:34:53 +0100

    make the damage calculator more closely resemble the multiplier system

--- domain/damage/calculator/MultiplierDamageCalculator.cs
+++ domain/damage/calculator/MultiplierDamageCalculator.cs
@@ -5,7 +5,7 @@ public class MultiplierDamageCalculator : IDamageCalculator
     private const int NoCombo = -1;
     private const int NoBackToBack = 0;
     private const int PerfectClearDamage = 10;
-    private static readonly double[] LineClearDamage = { 0, 0, 1, 2, 4};
+    private static readonly int[] LineClearDamage = { 0, 0, 1, 2, 4};
     private static readonly double[] ComboMultiplier = { 0, 0, 0.25, 0.5, 1};
     
     private int _combo = NoCombo;
@@ -28,33 +28,35 @@ public class MultiplierDamageCalculator : IDamageCalculator
     private int HandleLineClear(int linesCleared, SpinType spinType)
     {
         _combo++;
-        double resultDamage = GetLineClearDamage(linesCleared, spinType) 
-                           + GetComboDamage(linesCleared, spinType)
-                           * GetBackToBackMultiplier(linesCleared, spinType);
-        return (int)resultDamage;
+        int resultDamage = GetLineClearDamage(linesCleared, spinType)
+                              + GetBackToBackMultiplier(linesCleared, spinType)
+                              + GetComboDamage(linesCleared, spinType);
+        return resultDamage;
     }
 
-    private double GetLineClearDamage(int linesCleared, SpinType spinType)
+    private int GetLineClearDamage(int linesCleared, SpinType spinType)
     {
         if (spinType == SpinType.FullSpin) return linesCleared * 2;
         return LineClearDamage[Math.Min(linesCleared, LineClearDamage.Length - 1)];
     }
     
-    private double GetComboDamage(int linesCleared, SpinType spinType)
+    private int GetComboDamage(int linesCleared, SpinType spinType)
     {
-        if (linesCleared == 1) return Math.Log(1 + 1.25 * _combo, Math.E);
-        if (spinType == SpinType.FullSpin) return (int)spinType * 0.5 * _combo;
-        return ComboMultiplier[Math.Min(linesCleared, ComboMultiplier.Length - 1)] * _combo;
+        double damage;
+        if (linesCleared == 1) damage = Math.Log(1 + 1.25 * _combo, Math.E);
+        else if (spinType == SpinType.FullSpin) damage = ((int)spinType * 0.5 * _combo);
+        else damage = (ComboMultiplier[Math.Min(linesCleared, ComboMultiplier.Length - 1)] * _combo);
+        return (int)Math.Floor(damage);
     }
 
-    private double GetBackToBackMultiplier(int linesCleared, SpinType spinType)
+    private int GetBackToBackMultiplier(int linesCleared, SpinType spinType)
     {
         if (IsSpecialClear(linesCleared, spinType))
         {
             _backToBack++;
-            return Math.Log(_backToBack * 2.2, Math.E);
+            return (int)Math.Log(_backToBack * 2.2, Math.E);
         }
-        _backToBack = NoBackToBack;
+        if (linesCleared > 0) _backToBack = NoBackToBack;
         return 1;
     }
 
