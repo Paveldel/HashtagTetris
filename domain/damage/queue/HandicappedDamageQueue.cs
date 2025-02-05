43d133911fe6c3078cb43e7ac573e3665a0d0e9d
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 17:40:33 +0100

    create damage queue which gives a player a handicap

---
+++ domain/damage/queue/HandicappedDamageQueue.cs
@@ -0,0 +1,29 @@
+﻿namespace domain.damage.queue;
+
+public class HandicappedDamageQueue(IDamageQueue original, double multiplier) : IDamageQueue
+{
+    private double _leftOvers;
+    
+    public void RegisterAsReceiver(IDamageReceiver receiver)
+    {
+        original.RegisterAsReceiver(receiver);
+    }
+
+    public List<int> GetQueue()
+    {
+        return original.GetQueue();
+    }
+
+    public void PiecePlaced(int amountOfDamageSent, bool lineCleared)
+    {
+        original.PiecePlaced(amountOfDamageSent, lineCleared);
+    }
+
+    public void AddGarbageToQueue(int amountOfGarbage)
+    {
+        double multipliedDamage = amountOfGarbage * multiplier;
+        multipliedDamage += _leftOvers;
+        _leftOvers = multipliedDamage % 1;
+        original.AddGarbageToQueue((int)Math.Floor(multipliedDamage));
+    }
+}
