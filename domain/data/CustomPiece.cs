689efb0ba8883e41cfb5903abffc24e59b6cceed
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 11:26:21 +0100

    create custom piece implementation for pieces that change rotation center when rotating

---
+++ domain/data/CustomPiece.cs
@@ -0,0 +1,49 @@
+﻿namespace domain.data;
+
+public class CustomPiece(Block[][] blocks, PieceType pieceType, int rotIndex = 2, int x = 0, int y = 0) : IPiece
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
+    public int RotIndex { get; private set; } = rotIndex % blocks.Length;
+    private PieceType _pieceType = pieceType;
+
+    private Block[][] _blocks = blocks;
+
+    public Block[] GetBlocks()
+    {
+        return _blocks[RotIndex];
+    }
+
+    public int GetPieceIndex()
+    {
+        return (int)_pieceType;
+    }
+
+    public IPiece Clone()
+    {
+        return new CustomPiece(_blocks, _pieceType, RotIndex, X, Y);
+    }
+
+    public IPiece Rotate(Rotation rotation)
+    {
+        CustomPiece rotatePiece = (CustomPiece)Clone();
+        rotatePiece.RotIndex += (int)rotation;
+        rotatePiece.RotIndex = rotatePiece.RotIndex % _blocks.Length;
+        return rotatePiece;
+    }
+
+    public void MoveLeft()
+    {
+        X--;
+    }
+
+    public void MoveRight()
+    {
+        X++;
+    }
+
+    public void MoveDown()
+    {
+        Y--;
+    }
+}
