de104ef19bdf33fb50a8d40c584455925b424273
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 11:27:04 +0100

    create piece data that mimics Akira's rotation system

---
+++ domain/piecedata/ARSPieceData.cs
@@ -0,0 +1,60 @@
+﻿using domain.data;
+
+namespace domain.piecedata;
+
+public class ARSPieceData : IPieceData
+{
+    private static readonly IPiece[] Pieces = [
+        null!,
+        new CustomPiece(
+            new Block[][] {
+                [new Block(0, 0), new Block(1, 0), new Block(2, 0), new Block(-1, 0)],
+                [new Block(1, 0), new Block(1, 1), new Block(1, -1), new Block(1, -2)],
+            }, PieceType.I),
+        new CustomPiece(
+            new Block[][] {
+                [new Block(0, 0), new Block(0, -1), new Block(1, -1), new Block(-1, -1)],
+                [new Block(0, 0), new Block(1, 0), new Block(0, -1), new Block(0, 1)],
+                [new Block(0, 0), new Block(1, 0), new Block(0, -1), new Block(-1, 0)],
+                [new Block(0, 0), new Block(-1, 0), new Block(0, -1), new Block(0, 1)],
+            }, PieceType.T),
+        new CustomPiece(
+            new Block[][] {
+                [new Block(0, 0), new Block(0, -1), new Block(1, -1), new Block(1, 0)],
+            }, PieceType.O),
+        new CustomPiece(
+            new Block[][] {
+                [new Block(1, 0), new Block(0, -1), new Block(1, -1), new Block(-1, -1)],
+                [new Block(0, 0), new Block(1, -1), new Block(0, -1), new Block(0, 1)],
+                [new Block(0, 0), new Block(1, 0), new Block(-1, -1), new Block(-1, 0)],
+                [new Block(0, 0), new Block(-1, 1), new Block(0, -1), new Block(0, 1)],
+            }, PieceType.L),
+        new CustomPiece(
+            new Block[][] {
+                [new Block(-1, 0), new Block(0, -1), new Block(-1, -1), new Block(1, -1)],
+                [new Block(0, 0), new Block(-1, -1), new Block(0, -1), new Block(0, 1)],
+                [new Block(0, 0), new Block(-1, 0), new Block(1, -1), new Block(1, 0)],
+                [new Block(0, 0), new Block(1, 1), new Block(0, -1), new Block(0, 1)],
+            }, PieceType.J),
+        new CustomPiece(
+            new Block[][] {
+                [new Block(0, -1), new Block(-1, -1), new Block(0, 0), new Block(1, 0)],
+                [new Block(0, 0), new Block(0, -1), new Block(-1, 0), new Block(-1, 1)],
+            }, PieceType.S),
+        new CustomPiece(
+            new Block[][] {
+                [new Block(0, -1), new Block(1, -1), new Block(0, 0), new Block(-1, 0)],
+                [new Block(0, 0), new Block(0, -1), new Block(1, 0), new Block(1, 1)],
+            }, PieceType.Z),
+    ];
+    
+    public IPiece? GetPieceByIndex(int index)
+    {
+        return Pieces[index]?.Clone() ?? null;
+    }
+
+    public int GetAmountOfPieces()
+    {
+        return Pieces.Length - 1;
+    }
+}
