a5904caa67cc8b4a524407c24065cbcc33e5e97d
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 17:33:15 +0100

    create a fake damage queue which takes no garbage

---
+++ domain/damage/queue/FakeDamageQueue.cs
@@ -0,0 +1,15 @@
+﻿namespace domain.damage.queue;
+
+public class FakeDamageQueue : IDamageQueue
+{
+    public void RegisterAsReceiver(IDamageReceiver receiver) { }
+
+    public List<int> GetQueue()
+    {
+        return [];
+    }
+
+    public void PiecePlaced(int amountOfDamageSent, bool lineCleared) { }
+
+    public void AddGarbageToQueue(int amountOfGarbage) { }
+}
