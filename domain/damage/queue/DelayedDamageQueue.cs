2576d703791762011eb05dfd62fea570eb8d58b5
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 17:23:21 +0100

    create damage queue which delays when the damage is taken

---
+++ domain/damage/queue/DelayedDamageQueue.cs
@@ -0,0 +1,35 @@
+﻿using domain.timer;
+using ITimer = domain.timer.ITimer;
+
+namespace domain.damage.queue;
+
+public class DelayedDamageQueue(Board board, long waitTime, bool allowCanceling = true, bool allowBlocking = true) : DamageQueue(board, allowCanceling, allowBlocking), IUpdatable
+{
+    
+    private readonly List<long> _delays = [];
+    private ITimer _timer;
+
+    public override void AddGarbageToQueue(int amountOfGarbage)
+    {
+        base.AddGarbageToQueue(amountOfGarbage);
+        _delays.Add(_timer.GetCurrentTime() + waitTime);
+    }
+
+    protected override bool CanTakeDamage()
+    {
+        return base.CanTakeDamage() && _timer.GetCurrentTime() >= _delays[0];
+    }
+
+    protected override void TakeLineFromQueue()
+    {
+        base.TakeLineFromQueue();
+        if (_queue.Count < _delays.Count) _delays.RemoveAt(0);
+    }
+
+    public void Update(long currentTime) { }
+
+    public void SetTimer(ITimer timer)
+    {
+        _timer = timer;
+    }
+}
