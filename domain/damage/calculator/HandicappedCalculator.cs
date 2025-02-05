f961cab571ef7be8dfac060e350c378a9ed39341
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 16:53:52 +0100

    create handicap damage calculator which allows you to tweak damage output with a factor

---
+++ domain/damage/calculator/HandicappedCalculator.cs
@@ -0,0 +1,26 @@
+﻿namespace domain.damage.calculator;
+
+public class HandicappedCalculator(IDamageCalculator original, double multiplier) : IDamageCalculator
+{
+    private double _leftOvers;
+
+    public int CalculateDamage(int linesCleared, SpinType spinType, bool isPerfectClear)
+    {
+        int damage = original.CalculateDamage(linesCleared, spinType, isPerfectClear);
+        double multipliedDamage = damage * multiplier;
+        multipliedDamage += _leftOvers;
+        Console.WriteLine(_leftOvers);
+        _leftOvers = multipliedDamage % 1;
+        return (int)Math.Floor(multipliedDamage);
+    }
+
+    public int GetCombo()
+    {
+        return original.GetCombo();
+    }
+
+    public int GetBackToBack()
+    {
+        return original.GetBackToBack();
+    }
+}
