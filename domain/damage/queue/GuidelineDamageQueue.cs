a1be6e2e961003e8f3b6977ca4ffca7c3fb80dc8
pavel <pavel.deleersnyder@student.howest.be>
2025-02-05 17:30:20 +0100

    create a damage queue which matches the 8-line switch in the guideline

---
+++ domain/damage/queue/GuidelineDamageQueue.cs
@@ -0,0 +1,24 @@
+﻿namespace domain.damage.queue;
+
+public class GuidelineDamageQueue(Board board, bool allowCanceling = true, bool allowBlocking = true)
+    : AbstractGarbageQueue(board, allowCanceling, allowBlocking)
+{
+    private const int LinesBeforeSwitch = 8;
+    
+    private int _linesUntilSwitch = LinesBeforeSwitch;
+    
+    protected override void AfterTakeLine()
+    {
+        _linesUntilSwitch--;
+        if (_linesUntilSwitch == 0)
+        {
+            RandomiseWell();
+            _linesUntilSwitch = LinesBeforeSwitch;
+        }
+    }
+
+    protected override void AfterTakeBatch()
+    {
+        return;
+    }
+}
