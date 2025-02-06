f3e7baba3af33c7ac4f7282c3f9517a4d9288e22
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 22:14:01 +0100

    avoid dispatcher issues

--- GUI/Components/Pages/Versus.razor.cs
+++ GUI/Components/Pages/Versus.razor.cs
@@ -1,21 +1,40 @@
 ﻿using domain;
+using domain.timer;
 using domain.versus;
 using Microsoft.AspNetCore.Components;
+using ITimer = domain.timer.ITimer;
 
 namespace GUI.Components.Pages;
 
-public partial class Versus : ComponentBase, IParticipant
+public partial class Versus : ComponentBase, IParticipant, IUpdatable
 {
     private readonly VersusGame _versusGame;
     
     private Game? _localPlayer;
     private Game? _otherPlayer;
+    private ITimer _timer;
 
     public Versus()
     {
         _versusGame = GlobalVersusQueue.VersusQueue.GetGameToJoin();
         GlobalVersusQueue.VersusQueue.RegisterInGame(this);
+        UpdatePage();
     }
+    
+    private void UpdatePage()
+    {
+        _timer = new GameLoop();
+        _timer.RegisterUpdatable(this);
+        _timer.StartLoop();
+    }
+
+    public void Update(long currentTime)
+    {
+        if (_versusGame.HasGameStarted()) _timer.EndTimer();
+        StateHasChanged();
+    }
+
+    public void SetTimer(ITimer timer) { }
     
     public void GameStart(Game participantGame, int playerId)
     {
