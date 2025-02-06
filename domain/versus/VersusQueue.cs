59c7b174da06870c5328c11727c6154bed8b67b3
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 20:20:48 +0100

    create versusQueue object which can register participants in games

---
+++ domain/versus/VersusQueue.cs
@@ -0,0 +1,20 @@
+﻿namespace domain.versus;
+
+public class VersusQueue
+{
+    private VersusGame _unfilledGame = new();
+
+    public VersusGame RegisterInGame(IParticipant participant)
+    {
+        VersusGame versusGame = _unfilledGame;
+        versusGame.RegisterForGame(participant);
+        ReplaceFilledGame();
+        return versusGame;
+    }
+
+    private void ReplaceFilledGame()
+    {
+        if (!_unfilledGame.HasGameStarted()) return;
+        _unfilledGame = new VersusGame();
+    }
+}
