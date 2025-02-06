fc07618fab431f661207b574b7ea362b83ebcb4e
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 21:35:39 +0100

    create page for playing versus games

---
+++ GUI/Components/Pages/Versus.razor.cs
@@ -0,0 +1,25 @@
+﻿using domain;
+using domain.versus;
+using Microsoft.AspNetCore.Components;
+
+namespace GUI.Components.Pages;
+
+public partial class Versus : ComponentBase, IParticipant
+{
+    private readonly VersusGame _versusGame;
+    
+    private Game? _localPlayer;
+    private Game? _otherPlayer;
+
+    public Versus()
+    {
+        _versusGame = GlobalVersusQueue.VersusQueue.GetGameToJoin();
+        GlobalVersusQueue.VersusQueue.RegisterInGame(this);
+    }
+    
+    public void GameStart(Game participantGame, int playerId)
+    {
+        _localPlayer = participantGame;
+        _otherPlayer = _versusGame.GetOpponent(playerId);
+    }
+}
