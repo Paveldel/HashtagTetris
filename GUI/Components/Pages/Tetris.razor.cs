63bec718739eaac934ec963cd5cec92a0f5e4fa3
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 20:14:44 +0100

    apply rename in factory to page

--- GUI/Components/Pages/Tetris.razor.cs
+++ GUI/Components/Pages/Tetris.razor.cs
@@ -10,6 +10,6 @@ public partial class Tetris : ComponentBase
     public Game GetTetrisGame()
     {
         Config configuration = new Config();
-        return new GameFactory(configuration).CreateGame();
+        return new GameFactory(configuration).CreateSingleGame();
     }
 }
