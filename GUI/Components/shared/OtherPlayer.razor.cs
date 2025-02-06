0264e550b735cda3e598b34d32275ffaff2d61a7
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 16:56:29 +0100

    create component to render the boards op players that are not being controlled here

--- GUI/Components/shared/OtherPlayer.razor.cs
+++ GUI/Components/shared/OtherPlayer.razor.cs
@@ -1,29 +1,16 @@
 ﻿using domain;
-using domain.timer;
 using Microsoft.AspNetCore.Components;
-using ITimer = domain.timer.ITimer;
 
 namespace GUI.Components.shared;
 
-public partial class OtherPlayer : ComponentBase, IUpdatable
+public partial class OtherPlayer : ComponentBase
 {
     [Parameter]
     public Game Game { get; set; }
-    
-    protected override void OnAfterRender(bool firstRender)
-    {
-        if (firstRender)
-        {
-            Game.StartGame(this);
-        }
-    }
 
     public void Update(long currentTime)
     {
-        Game.Renderer.UpdateBoardToRender();
         StateHasChanged();
     }
-
-    public void SetTimer(ITimer timer) { }
     
 }
