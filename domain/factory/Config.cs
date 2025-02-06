c25266c6717987096d0e2728fc281c95fa7883fc
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 11:04:43 +0100

    create Config Object which Holds all the settings the player can change

---
+++ domain/factory/Config.cs
@@ -0,0 +1,22 @@
+﻿namespace domain.factory;
+
+public class Config
+{
+    public int DamageCalculator { get; set; } = 0;
+    public double DamageSentMultiplier { get; set; } = 1;
+    public int DamageQueue { get; set; } = 0;
+    public double DamageTakenMultiplier { get; set; } = 1;
+    public int HoldType { get; set; } = 0;
+    public int AmountOfPreviews { get; set; } = 5;
+    public long AppearanceDelay { get; set; } = 0;
+    public long LineClearDelay { get; set; } = 0;
+    public int PieceQueue { get; set; } = 0;
+    public int RotationSystem { get; set; } = 0;
+    public int PieceData { get; set; } = 0;
+    public int SpinDetector { get; set; } = 0;
+    public int Gravity { get; set; } = 500;
+    public int Resets { get; set; } = 10;
+    public int BoardWidth { get; set; } = 10;
+    public int PieceHeight { get; set; } = 20;
+    public bool ShowShadowPiece { get; set; } = true;
+}
