580161e3ab0e9414eaacfbe2e206d9283d1ab596
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 20:10:35 +0100

    create timers for versus games

---
+++ domain/timer/VersusTimer.cs
@@ -0,0 +1,38 @@
+﻿namespace domain.timer;
+
+public class VersusTimer(MasterTimer master) : ITimer
+{
+    private readonly List<IUpdatable> _updatables = new (); 
+    
+    public long GetCurrentTime()
+    {
+        return master.GetCurrentTime();
+    }
+
+    public void RegisterUpdatable(params IUpdatable[] updatables)
+    {
+        foreach (var updatable in updatables)
+        {
+            updatable.SetTimer(this);
+            _updatables.Add(updatable);
+        }
+    }
+
+    public void EndTimer()
+    {
+        master.PlayerDied(this);
+    }
+
+    public void Update(long currentTime)
+    {
+        foreach (var updatable in _updatables)
+        {
+            updatable.Update(currentTime);
+        }
+    }
+    
+    public Task StartLoop()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ domain/versus/VersusGame.cs
@@ -0,0 +1,38 @@
+﻿using domain.factory;
+using domain.timer;
+
+namespace domain.versus;
+
+public class VersusGame
+{
+    private IParticipant? _playerOne;
+    private IParticipant? _playerTwo;
+
+    private Game? _gameOne;
+    private Game? _gameTwo;
+    
+    public void RegisterForGame(IParticipant participant)
+    {
+        if (_playerOne == null) _playerOne = participant;
+        else if (_playerTwo == null)
+        {
+            _playerTwo = participant;
+            StartGame();
+        }
+    }
+
+    private void StartGame()
+    {
+        MasterTimer timer = new MasterTimer();
+        GameFactory factory = new GameFactory(GetVersusConfig());
+        _gameOne = factory.CreateMultiPlayerGame(timer);
+        _gameTwo = factory.CreateMultiPlayerGame(timer);
+        _playerOne!.GameStart(_gameOne, 1);
+        _playerTwo!.GameStart(_gameTwo, 2);
+    }
+
+    private static Config GetVersusConfig()
+    {
+        return new Config();
+    }
+}
