cddf201a0ea87eadcde18b42e99cfa844b3350b2
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 20:54:03 +0100

    make other player update itself automatically

--- GUI/Components/shared/OtherPlayer.razor.cs
+++ GUI/Components/shared/OtherPlayer.razor.cs
@@ -1,9 +1,11 @@
 ﻿using domain;
+using domain.timer;
 using Microsoft.AspNetCore.Components;
+using ITimer = domain.timer.ITimer;
 
 namespace GUI.Components.shared;
 
-public partial class OtherPlayer : ComponentBase
+public partial class OtherPlayer : ComponentBase, IUpdatable
 {
     [Parameter]
     public Game Game { get; set; }
@@ -13,4 +15,20 @@ public partial class OtherPlayer : ComponentBase
         StateHasChanged();
     }
     
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            UpdateBoard();
+        }
+    }
+
+    private void UpdateBoard()
+    {
+        ITimer timer = new GameLoop();
+        timer.RegisterUpdatable(this);
+        timer.StartLoop();
+    }
+
+    public void SetTimer(ITimer timer) { }
 }
