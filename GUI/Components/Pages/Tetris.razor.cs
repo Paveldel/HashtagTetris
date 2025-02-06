5cd63126937808971ee95d1084f73d13ebd079ed
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 14:56:21 +0100

    move the tetris game into its own component so it can be reused for different purposes

--- GUI/Components/Pages/Tetris.razor.cs
+++ GUI/Components/Pages/Tetris.razor.cs
@@ -2,47 +2,15 @@
 using domain.factory;
 using domain.timer;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using ITimer = domain.timer.ITimer;
 
 namespace GUI.Components.Pages;
 
-public partial class Tetris : ComponentBase, IUpdatable
+public partial class Tetris : ComponentBase
 {
-    public Tetris()
+    
+    public Game GetTetrisGame()
     {
         Config configuration = new Config();
-        Game = new GameFactory(configuration).CreateGame();
+        return new GameFactory(configuration).CreateGame();
     }
-
-    private Game Game { get; set; }
-
-    protected async void ButtonReleased(KeyboardEventArgs e)
-    {
-        Game.HandleKeyRelease(e.Code);
-        Game.Renderer.UpdateBoardToRender();
-    }
-
-    protected async void ButtonPressed(KeyboardEventArgs e)
-    {
-        if (e.Repeat) return;
-        Game.HandleKeyPress(e.Code);
-        Game.Renderer.UpdateBoardToRender();
-    }
-    
-    protected override void OnAfterRender(bool firstRender)
-    {
-        if (firstRender)
-        {
-            Game.StartGame(this);
-        }
-    }
-
-    public void Update(long currentTime)
-    {
-        Game.Renderer.UpdateBoardToRender();
-        StateHasChanged();
-    }
-
-    public void SetTimer(ITimer timer) { }
 }
---
+++ GUI/Components/shared/LocalPlayer.razor.cs
@@ -0,0 +1,43 @@
+﻿using domain;
+using domain.timer;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using ITimer = domain.timer.ITimer;
+
+namespace GUI.Components.shared;
+
+public partial class LocalPlayer : ComponentBase, IUpdatable
+{
+    [Parameter]
+    public Game Game { get; set; }
+    
+    protected async void ButtonReleased(KeyboardEventArgs e)
+    {
+        Game.HandleKeyRelease(e.Code);
+        Game.Renderer.UpdateBoardToRender();
+    }
+
+    protected async void ButtonPressed(KeyboardEventArgs e)
+    {
+        if (e.Repeat) return;
+        Game.HandleKeyPress(e.Code);
+        Game.Renderer.UpdateBoardToRender();
+    }
+    
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Game.StartGame(this);
+        }
+    }
+
+    public void Update(long currentTime)
+    {
+        Game.Renderer.UpdateBoardToRender();
+        StateHasChanged();
+    }
+
+    public void SetTimer(ITimer timer) { }
+    
+}
