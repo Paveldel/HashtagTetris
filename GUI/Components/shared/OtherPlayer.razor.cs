6404ae707384c647be9e234947dfaf8b74a12ca2
pavel <pavel.deleersnyder@student.howest.be>
2025-02-06 15:04:34 +0100

    create other player component which can render non-local players

---
+++ GUI/Components/shared/OtherPlayer.razor.cs
@@ -0,0 +1,29 @@
+﻿using domain;
+using domain.timer;
+using Microsoft.AspNetCore.Components;
+using ITimer = domain.timer.ITimer;
+
+namespace GUI.Components.shared;
+
+public partial class OtherPlayer : ComponentBase, IUpdatable
+{
+    [Parameter]
+    public Game Game { get; set; }
+    
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Game.StartGame(this);
+        }
+    }
+
+    public void Update(long currentTime)
+    {
+        Game.Renderer.UpdateBoardToRender();
+        StateHasChanged();
+    }
+
+    public void SetTimer(ITimer timer) { }
+    
+}
